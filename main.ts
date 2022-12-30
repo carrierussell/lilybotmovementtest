18b19a8c39329680891bb5056b1bef5346585fe8
carrierussell <77681170+carrierussell@users.noreply.github.com>
2022-12-30 13:02:42 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 namespace Lilybot.movement {
     //%blockId=Lilybot_MovementB
-    //%block="Backward at $motorSpeedB speed for $motorTimeB"
+    //%block="Backward at $motorSpeedB speed for $motorTimeB sec"
     //%motorSpeedB.min=0 motorSpeedB.max=100
     //%motorTimeB.min=0 motorTimeB.max=10 
 export function goBackward(motorSpeedB: number, motorTimeB: number) {
@@ -19,7 +19,7 @@ export function goBackward(motorSpeedB: number, motorTimeB: number) {
     pins.analogWritePin(AnalogPin.P1, 0)
 }
  //%blockId=Lilybot_MovementF
-    //%block="Forward at $motorSpeedF speed for $motorTimeF"
+    //%block="Forward at $motorSpeedF speed for $motorTimeF sec"
     //%motorSpeedF.min=0 motorSpeedF.max=100
     //%motorTimeF.min=0 motorTimeF.max=10 
 export function goForward(motorSpeedF: number, motorTimeF: number) {
@@ -39,16 +39,16 @@ export function goForward(motorSpeedF: number, motorTimeF: number) {
 }
 //%blockId=Lilybot_MovementR
     //%block="Right at $motorSpeedR speed for $motorTimeR sec"
-    //%motorSpeedR.min=0 motorTimeR.max=100
-    //%motorSpeedR.min=0 motorTimeR.max=10 
-export function goRight(motorSpeed3: number, motorTime3: number) {
-    pins.analogWritePin(AnalogPin.P0, motorSpeed3)
+    //%motorSpeedR.min=0 motorSpeedR.max=100
+    //%motorTimeR.min=0 motorTimeR.max=10 
+export function goRight(motorSpeedR: number, motorTimeR: number) {
+    pins.analogWritePin(AnalogPin.P0, motorSpeedR)
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P13, 1)
-    pins.analogWritePin(AnalogPin.P1, motorSpeed3)
+    pins.analogWritePin(AnalogPin.P1, motorSpeedR)
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P16, 0)
-    basic.pause(motorTime3)
+    basic.pause(motorTimeR)
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P9, 0)
     pins.analogWritePin(AnalogPin.P0, 0)
@@ -57,18 +57,17 @@ export function goRight(motorSpeed3: number, motorTime3: number) {
     pins.analogWritePin(AnalogPin.P1, 0)
 }
     //%blockId=Lilybot_MovementL
-    //%block="Left at $index speed for"
-    //%index.min=0 index.max=100
-    //% "$secindex "
-    //%secindex.min=0 secindex.max=10 
-    export function goLeft(motorSpeed3: number, motorTime3: number) {
-        pins.analogWritePin(AnalogPin.P0, motorSpeed3)
+    //%block="Left at $motorSpeedL speed for $motorTimeL sec"
+    //%motorSpeedL.min=0 motorSpeedL.max=100
+    //%motorTimeL.min=0 motorTimeL.max=10 
+    export function goLeft(motorSpeedL: number, motorTimeL: number) {
+        pins.analogWritePin(AnalogPin.P0, motorSpeedL)
         pins.digitalWritePin(DigitalPin.P8, 1)
         pins.digitalWritePin(DigitalPin.P13, 0)
-        pins.analogWritePin(AnalogPin.P1, motorSpeed3)
+        pins.analogWritePin(AnalogPin.P1, motorSpeedL)
         pins.digitalWritePin(DigitalPin.P15, 0)
         pins.digitalWritePin(DigitalPin.P16, 1)
-        basic.pause(motorTime3)
+        basic.pause(motorTimeL)
         pins.digitalWritePin(DigitalPin.P8, 0)
         pins.digitalWritePin(DigitalPin.P9, 0)
         pins.analogWritePin(AnalogPin.P0, 0)
