6eeb59ad65875e05d121db4a2b38eea3be34eadf
carrierussell <77681170+carrierussell@users.noreply.github.com>
2022-12-30 11:45:20 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 namespace Lilybot.movement{
+    //%blockId=lilybot
+    //%block="Backward at $index speed for $secindex seconds"
+    //%index.min=0 index.max=100
+    //%secindex.min=0 secindex.max=10
 export function goBackward(motorSpeed: number, motorTime: number) {
     pins.analogWritePin(AnalogPin.P0, motorSpeed)
     pins.digitalWritePin(DigitalPin.P8, 0)
@@ -15,14 +19,14 @@ export function goBackward(motorSpeed: number, motorTime: number) {
     pins.analogWritePin(AnalogPin.P1, 0)
 }
 
-function goForward(motorSpeed2: number, motorTime2: number) {
+export function goForward(motorSpeed2: number, motorTime: number) {
     pins.analogWritePin(AnalogPin.P0, motorSpeed2)
     pins.digitalWritePin(DigitalPin.P8, 1)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.analogWritePin(AnalogPin.P1, motorSpeed2)
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P16, 0)
-    basic.pause(motorTime2)
+    basic.pause(motorTime)
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P9, 0)
     pins.analogWritePin(AnalogPin.P0, 0)
@@ -31,14 +35,14 @@ function goForward(motorSpeed2: number, motorTime2: number) {
     pins.analogWritePin(AnalogPin.P1, 0)
 }
 
-function goRight(motorSpeed3: number, motorTime3: number) {
+export function goRight(motorSpeed3: number, motorTime: number) {
     pins.analogWritePin(AnalogPin.P0, motorSpeed3)
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.analogWritePin(AnalogPin.P1, motorSpeed3)
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P16, 0)
-    basic.pause(motorTime3)
+    basic.pause(motorTime)
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P9, 0)
     pins.analogWritePin(AnalogPin.P0, 0)
@@ -46,6 +50,21 @@ function goRight(motorSpeed3: number, motorTime3: number) {
     pins.digitalWritePin(DigitalPin.P16, 0)
     pins.analogWritePin(AnalogPin.P1, 0)
 }
+    export function goLeft(motorSpeed3: number, motorTime: number) {
+        pins.analogWritePin(AnalogPin.P0, motorSpeed3)
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.analogWritePin(AnalogPin.P1, motorSpeed3)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
+        basic.pause(motorTime)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P9, 0)
+        pins.analogWritePin(AnalogPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        pins.analogWritePin(AnalogPin.P1, 0)
+    }
 }
 
 
