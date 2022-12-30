87a7c580259dd6d9828bbe724615830e9af5fd3b
Carrie Russell <77681170+carrierussell@users.noreply.github.com>
2022-12-30 15:45:10 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//%color=#00008b icon="/uf48b"
+//%color=#00008b icon="\uf48b"
 namespace Lilybot.movement {
     //%blockId=Lilybot_MovementB
     //%block="Backward at $motorSpeedB speed for $motorTimeB sec"
