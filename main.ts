43188e86e7d1af6eeed12aa1223205449bf6346e
carrierussell <77681170+carrierussell@users.noreply.github.com>
2022-12-30 12:02:52 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -19,14 +19,14 @@ export function goBackward(motorSpeed: number, motorTime: number) {
     pins.analogWritePin(AnalogPin.P1, 0)
 }
 
-export function goForward(motorSpeed2: number, motorTime: number) {
+function goForward(motorSpeed2: number, motorTime2: number) {
     pins.analogWritePin(AnalogPin.P0, motorSpeed2)
     pins.digitalWritePin(DigitalPin.P8, 1)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.analogWritePin(AnalogPin.P1, motorSpeed2)
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P16, 0)
-    basic.pause(motorTime)
+    basic.pause(motorTime2)
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P9, 0)
     pins.analogWritePin(AnalogPin.P0, 0)
@@ -35,14 +35,17 @@ export function goForward(motorSpeed2: number, motorTime: number) {
     pins.analogWritePin(AnalogPin.P1, 0)
 }
 
-export function goRight(motorSpeed3: number, motorTime: number) {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    goForward(1, 1)
+})
+function goRight(motorSpeed3: number, motorTime3: number) {
     pins.analogWritePin(AnalogPin.P0, motorSpeed3)
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.analogWritePin(AnalogPin.P1, motorSpeed3)
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P16, 0)
-    basic.pause(motorTime)
+    basic.pause(motorTime3)
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P9, 0)
     pins.analogWritePin(AnalogPin.P0, 0)
@@ -50,21 +53,5 @@ export function goRight(motorSpeed3: number, motorTime: number) {
     pins.digitalWritePin(DigitalPin.P16, 0)
     pins.analogWritePin(AnalogPin.P1, 0)
 }
-    export function goLeft(motorSpeed3: number, motorTime: number) {
-        pins.analogWritePin(AnalogPin.P0, motorSpeed3)
-        pins.digitalWritePin(DigitalPin.P8, 1)
-        pins.digitalWritePin(DigitalPin.P13, 0)
-        pins.analogWritePin(AnalogPin.P1, motorSpeed3)
-        pins.digitalWritePin(DigitalPin.P15, 0)
-        pins.digitalWritePin(DigitalPin.P16, 1)
-        basic.pause(motorTime)
-        pins.digitalWritePin(DigitalPin.P8, 0)
-        pins.digitalWritePin(DigitalPin.P9, 0)
-        pins.analogWritePin(AnalogPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P15, 0)
-        pins.digitalWritePin(DigitalPin.P16, 0)
-        pins.analogWritePin(AnalogPin.P1, 0)
-    }
 }
-
 
