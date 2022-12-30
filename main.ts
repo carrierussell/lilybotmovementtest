982cd603d31d9af348909adb5e156691322118fa
carrierussell <77681170+carrierussell@users.noreply.github.com>
2022-12-30 12:09:57 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
-namespace Lilybot.movement{
-    //%blockId=lilybot
-    //%block="Backward at $index speed for $secindex seconds"
+namespace Lilybot.movement {
+    //%blockId=Lilybot_Movement
+    //%block="Backward at $index 
     //%index.min=0 index.max=100
-    //%secindex.min=0 secindex.max=10
+    //% speed"
+    //%secindex.min=0 secindex.max=10 
 export function goBackward(motorSpeed: number, motorTime: number) {
     pins.analogWritePin(AnalogPin.P0, motorSpeed)
     pins.digitalWritePin(DigitalPin.P8, 0)
@@ -19,7 +20,7 @@ export function goBackward(motorSpeed: number, motorTime: number) {
     pins.analogWritePin(AnalogPin.P1, 0)
 }
 
-function goForward(motorSpeed2: number, motorTime2: number) {
+export function goForward(motorSpeed2: number, motorTime2: number) {
     pins.analogWritePin(AnalogPin.P0, motorSpeed2)
     pins.digitalWritePin(DigitalPin.P8, 1)
     pins.digitalWritePin(DigitalPin.P13, 0)
@@ -35,10 +36,7 @@ function goForward(motorSpeed2: number, motorTime2: number) {
     pins.analogWritePin(AnalogPin.P1, 0)
 }
 
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    goForward(1, 1)
-})
-function goRight(motorSpeed3: number, motorTime3: number) {
+export function goRight(motorSpeed3: number, motorTime3: number) {
     pins.analogWritePin(AnalogPin.P0, motorSpeed3)
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P13, 1)
