1f9da60aa0f6facd5352e092bc84e8c47e7d21ae
carrierussell <77681170+carrierussell@users.noreply.github.com>
2022-12-30 12:34:04 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -39,7 +39,11 @@ export function goForward(motorSpeed2: number, motorTime2: number) {
     pins.digitalWritePin(DigitalPin.P16, 0)
     pins.analogWritePin(AnalogPin.P1, 0)
 }
-
+//%blockId=Lilybot_Movement
+    //%block="Right at $index speed for"
+    //%index.min=0 index.max=100
+    //% "$secindex "
+    //%secindex.min=0 secindex.max=10 
 export function goRight(motorSpeed3: number, motorTime3: number) {
     pins.analogWritePin(AnalogPin.P0, motorSpeed3)
     pins.digitalWritePin(DigitalPin.P8, 0)
@@ -55,5 +59,25 @@ export function goRight(motorSpeed3: number, motorTime3: number) {
     pins.digitalWritePin(DigitalPin.P16, 0)
     pins.analogWritePin(AnalogPin.P1, 0)
 }
+    //%blockId=Lilybot_Movement
+    //%block="Left at $index speed for"
+    //%index.min=0 index.max=100
+    //% "$secindex "
+    //%secindex.min=0 secindex.max=10 
+    export function goLeft(motorSpeed3: number, motorTime3: number) {
+        pins.analogWritePin(AnalogPin.P0, motorSpeed3)
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.analogWritePin(AnalogPin.P1, motorSpeed3)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
+        basic.pause(motorTime3)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P9, 0)
+        pins.analogWritePin(AnalogPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        pins.analogWritePin(AnalogPin.P1, 0)
+    }
 }
 
